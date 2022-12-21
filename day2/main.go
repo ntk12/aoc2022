5e502494b933e0ba950fc4fd0cf89c59332f3f4a
lotusirous <5105237+lotusirous@users.noreply.github.com>
2022-12-21 14:46:31 +0700

    add day2 game 2

--- day2/main.go
+++ day2/main.go
@@ -13,6 +13,51 @@ const (
 	scissors = 3
 )
 
+const (
+	win  = 6
+	draw = 3
+	lost = 0
+)
+
+func Game2(op, end string) int {
+	// A for Rock, B for Paper, and C for Scissors
+
+	// X means you need to lose,
+	// Y means you need to end the round in a draw,
+	// Z means you need to win
+	switch op {
+	case "A": // rock
+		switch end {
+		case "X": // lose, you need to choose paper
+			return lost + scissors
+		case "Y": // draw, you need to choose rock
+			// you are rock
+			return draw + rock
+		case "Z": // win, you need to choose paper
+			return win + paper
+		}
+	case "B": // paper
+		switch end {
+		case "X": // lost
+			return lost + rock
+		case "Y": // draw
+			return draw + paper
+		case "Z": // win
+			return win + scissors
+		}
+	case "C": // scissors
+		switch end {
+		case "X": // lose
+			return lost + paper
+		case "Y": // draw
+			return draw + scissors
+		case "Z": // win
+			return win + rock
+		}
+	}
+	return 0
+}
+
 func Game(op, you string) int {
 	var turnScore int
 	switch you {
@@ -25,12 +70,6 @@ func Game(op, you string) int {
 	default:
 		panic("invalid value")
 	}
-
-	const (
-		win  = 6
-		draw = 3
-		lost = 0
-	)
 
 	// A for Rock, B for Paper, and C for Scissors
 	// X for Rock, Y for Paper, and Z for Scissors
@@ -67,7 +106,7 @@ func Game(op, you string) int {
 }
 
 func main() {
-	f, err := os.Open("./input1.txt")
+	f, err := os.Open("./input2.txt")
 	if err != nil {
 		log.Fatalf("Unable to read file: %v", err)
 	}
@@ -84,7 +123,7 @@ func main() {
 			continue
 		}
 		op, you := words[0], words[1]
-		total += Game(op, you)
+		total += Game2(op, you)
 	}
 
 	log.Printf("Score is: %d\n", total)
--- day2/main_test.go
+++ day2/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGame(t *testing.T) {
 	cases := []struct {
@@ -29,6 +31,37 @@ func TestGame(t *testing.T) {
 		t.Run("", func(t *testing.T) {
 			if got := Game(tt.op, tt.you); got != tt.want {
 				t.Errorf("Game(%s, %s) = %v, want %v", tt.op, tt.you, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGame2(t *testing.T) {
+	tests := []struct {
+		op   string
+		end  string
+		want int
+	}{
+		{
+			op:   "A",
+			end:  "Y",
+			want: 4,
+		},
+		{
+			op:   "B",
+			end:  "X",
+			want: 1,
+		},
+		{
+			op:   "C",
+			end:  "Z",
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := Game2(tt.op, tt.end); got != tt.want {
+				t.Errorf("Game2(%s, %s) = %v, want %v", tt.op, tt.end, got, tt.want)
 			}
 		})
 	}
